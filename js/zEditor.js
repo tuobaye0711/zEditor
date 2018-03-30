735270dec77bdeffc8c4c0a68ce83aa50257ea6e
zhleven <tuobaye0711@163.com>
2018-03-30 17:04:04 +0800

    js format

--- js/zEditor.js
+++ js/zEditor.js
@@ -1,3 +1,3 @@
-const changeStyle = (data) => {
-    data.value? document.execCommand(data.command, false, data.value):document.execCommand(data.command, false, null)
+const changeStyle = data => {
+    data.value ? document.execCommand(data.command, false, data.value) : document.execCommand(data.command, false, null)
 }
